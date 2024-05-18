fdc285d3910b45ac047f46200d6cfb23f619d43e
YankielYong <yankielyongmartinez@gmail.com>
2024-05-17 22:19:56 -0400

    Se agrego una validacion al dni

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -23,7 +23,7 @@ export class UserService {
 
   async create(createUserDto: CreateUserDto) {
     try {
-      const { email, password, name, ...userDetails } = createUserDto;
+      const { email, password, name, dni, ...userDetails } = createUserDto;
       const userExists = await this.userRepository.findOneBy({ email });
 
       if (userExists) {
@@ -31,11 +31,15 @@ export class UserService {
           message: 'Email already exists',
         });
       }
+
+      if (!this.validDni(dni)) throw new BadRequestException('Invalid dni');
+
       await this.mailService.sendWelcomeEmail(email, name);
       const user = this.userRepository.create({
         ...userDetails,
         email,
         name,
+        dni,
         password: hashSync(password, 10),
       });
 
@@ -95,10 +99,39 @@ export class UserService {
     return user;
   }
 
+  private validDni(dni: string): boolean {
+    // eslint-disable-next-line prettier/prettier
+    const months = ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12'];
+    const months28 = ['02'];
+    const months30 = ['04', '06', '09', '11'];
+    const months31 = ['01', '03', '05', '07', '08', '10', '12'];
+    // eslint-disable-next-line prettier/prettier
+    const days28 = ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25', '26', '27', '28'];
+    // eslint-disable-next-line prettier/prettier
+    const days30 = ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25', '26', '27', '28', '29', '30'];
+    // eslint-disable-next-line prettier/prettier
+    const days31 = ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25', '26', '27', '28', '29', '30', '31'];
+
+    const month = dni.slice(2, 4);
+    const day = dni.slice(4, 6);
+
+    if (!months.includes(month)) return false;
+
+    if (months28.includes(month) && !days28.includes(day)) return false;
+
+    if (months30.includes(month) && !days30.includes(day)) return false;
+
+    if (months31.includes(month) && !days31.includes(day)) return false;
+
+    return true;
+  }
+
   private handleDBErrors(error: any): never {
     if (error.code === '23505') throw new BadRequestException(error.detail);
     if (error.message === 'Email already exists')
       throw new ConflictException(error.message);
+    if (error.message === 'Invalid dni')
+      throw new BadRequestException(error.message);
 
     console.log(error);
 
